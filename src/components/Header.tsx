01d94b23c8ef4717dc32eece2b5ecdad7c046eff
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2025-07-02 02:29:42 +0400

    updating

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -110,7 +110,7 @@ export const Header = ({ setIsOpened }: { setIsOpened: (isOpen: boolean) => void
         </div>
       </div>
 
-      {/* Mobile Sidebar Menuww */}
+      {/* Mobile Sidebar Menuww## */}
       <AnimatePresence>
         {isMenuOpen && (
           <motion.div
